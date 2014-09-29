b4dfc13f97ccf9a7df52712f01011c9b0f2065e1
Christopher Bero <csb0019@uah.edu>
2014-09-29 13:46:37 -0500

    added framework for koPDF

---
+++ koPDF/bin/generate.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#
+# generate.sh
+# koPDF main script
+# This script will create a menu.json file based on available pdf files in /mnt/us/documents
+# To be used with koreader on Kindle PaperWhite 2013 (pw2)
+# Script author: Christopher Bero [bigbero@gmail.com]
